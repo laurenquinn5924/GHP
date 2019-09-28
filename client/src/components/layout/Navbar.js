cd2566d20adef91ea76f9bf002e1e55b3f7f5830
Lauren Hodges <laurenquinn5924@gmail.com>
2019-09-28 12:15:26 -0700

    Added landing page and made register, login routes functional

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,29 @@
+//racf will create functional component, will take name of component from file name
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+	return (
+		<nav className="navbar bg-dark">
+      <h1>
+				<Link to='/'>
+					<i className="fas fa-code"></i> Green Healing Project
+				</Link>
+        
+      </h1>
+      <ul>
+        <li>
+					<a href="profiles.html">Developers</a>
+				</li>
+        <li>
+					<Link to='/register'>Register</Link>
+				</li>
+        <li>
+					<Link to='/login'>Login</Link>
+				</li>
+      </ul>
+    </nav>
+	)
+}
+
+export default Navbar
